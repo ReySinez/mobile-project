3c03d920daba1de02d1af468a6fd94d671915bc4
ReySinez <91934375+ReySinez@users.noreply.github.com>
2024-06-03 23:24:37 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\reysa\\Desktop\\Photo-App-Project-Flutter-main\\lib\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\reysa\\Desktop\\planT-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\reysa\\Desktop\\Photo-App-Project-Flutter-main\\lib\\flutter"
+  "PROJECT_DIR=C:\\Users\\reysa\\Desktop\\planT-main"
+  "FLUTTER_ROOT=C:\\Users\\reysa\\Desktop\\Photo-App-Project-Flutter-main\\lib\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\reysa\\Desktop\\planT-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\reysa\\Desktop\\planT-main"
+  "FLUTTER_TARGET=C:\\Users\\reysa\\Desktop\\planT-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC81NWVhZTY4NjRiMjk2ZGQ5ZjQzYjJjYzc1NzdlYzI1NmU1YzMyYThkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\reysa\\Desktop\\planT-main\\.dart_tool\\package_config.json"
+)
